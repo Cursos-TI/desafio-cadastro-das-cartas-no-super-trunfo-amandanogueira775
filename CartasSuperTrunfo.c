71cd3b3ea33caeaed16b20d308aca41cb9280717
Amanda Nogueira <anunesnss@gmail.com>
2025-08-24 00:23:57 +0000

    Fiz as alterações conforme o nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-//Nível novato
+//Definindo variáveis
 int main() {
     int carta1; 
     int populacao1, populacao2;
@@ -11,12 +11,16 @@ int main() {
     char codigo1 [4] = "A01", codigo2 [4] = "A02"; //3 posições úteis + uma posição para o terminador "\0"
     char cidade1 [20], cidade2 [20];
 
-    //Nível aventureiro
+    //Adicionei 4 variáveis do nível aventureiro
     float densidadepopulacional1, densidadepopulacional2;
     float pibpercapita1, pibpercapita2;
 
-    // sintaxe scanf
-    // scanf("formato1""formato2, %variavel1, varialvel2, ...");
+    //Adicionei 11 variáveis do nível mestre
+    float inversodensidadepopulacional1, inversodensidadepopulacional2;
+
+    float superpoder1, superpoder2;
+
+    int populacao, area, pib, pontosturisticos, densidadepopulacional, pibpercapita, superpoder;
 
     //Inserir dados da primeira carta
     printf("*** Carta 1 *** \n");
@@ -48,11 +52,21 @@ int main() {
     printf("PIB: %f\nPontos Turísticos: %d\n", pib1, pontosturisticos1);
 
     //Nível aventureiro
+    // Operação para calcular a desnidade polpulacional e o PIB per capita da primeira carta
     densidadepopulacional1 = populacao1 / area1;
     pibpercapita1 = pib1 / populacao1;
 
+    //Dados do cálculo impresso
     printf("Densidade Populacional: %.2f\n", densidadepopulacional1);
     printf("PIB per Capita: %.2f\n", pibpercapita1);
+
+    //Nível mestre
+    // Cálculo do super poder
+    inversodensidadepopulacional1 = (float)1 / densidadepopulacional1; // Quanto menor a densidade, maior o poder
+    superpoder1 = (float)populacao1 + area1 + pib1 + pontosturisticos1 + pibpercapita1 + inversodensidadepopulacional1;
+
+    // resultado do super poder
+    printf("O super poder da carta 1 é: %f\n", superpoder1);
 
      //Inserir dados da segunda carta
     printf("*** Carta 2 *** \n");
@@ -84,13 +98,44 @@ int main() {
     printf("PIB: %f\nPontos Turísticos: %d\n", pib2, pontosturisticos2);
 
     //Nível aventureiro
+    // Operação para calcular a desnidade polpulacional e o PIB per capita da segunada carta
     densidadepopulacional2 = populacao2 / area2;
     pibpercapita2 = pib2 / populacao2;
 
     printf("Densidade Populacional: %.2f\n", densidadepopulacional2);
     printf("PIB per Capita: %.2f\n", pibpercapita2);
 
-    
+    // Nível mestre
+    // Cálculo do super poder
+    inversodensidadepopulacional2 = (float)1 / densidadepopulacional1; // Quanto menor a densidade, maior o poder
+    superpoder2 = (float)populacao2 + area2 + pib2 + pontosturisticos2 + pibpercapita2 + inversodensidadepopulacional2;
 
+    // resultado do super poder
+    printf("O super poder da carta 2 é: %f\n", superpoder2);
+
+    // Nível mestre
+    // Comparação das cartas
+    populacao = populacao1 > populacao2;
+    area = area1 > area2;
+    pib = pib1 > pib2;
+    pontosturisticos = pontosturisticos1 > pontosturisticos2;
+    densidadepopulacional = densidadepopulacional1 < densidadepopulacional2;//Densidade de menor valor vence
+    pibpercapita = pibpercapita1 > pibpercapita2;
+    superpoder = superpoder1 > superpoder2;
+
+    // imprimir resultado das comparações
+    printf("*** Comparação de Cartas***\n");
+    printf("População: %d\n", populacao);
+    printf("Área: %d\n", area);
+    printf("PIB: %d\n", pib);
+    printf("Pontos turísticos: %d\n", pontosturisticos);
+    printf("Densidade populacional: %d\n", densidadepopulacional);
+    printf("PIB per Capita: %d\n", pibpercapita);
+    printf("Super poder: %d\n", superpoder);
+
+
+    printf("Se o valor de cada variável for 1 - a carta 1 venceu, se o valor for 0 - a carta 2 venceu");
+
+    
     return 0;
 }
